f0ab09d0beb1c5ee8ee4b0e603e65a5fcb698339
Ayvenou <axel.yvenou@gmail.com>
2023-11-12 17:38:05 +0100

    initialize env variables and database connection

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vnaxel/go-jwt/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
 
 func main() {
-	fmt.Println("init")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
